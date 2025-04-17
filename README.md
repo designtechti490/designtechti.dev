a42ea9b6504890ea8843aee250315045da69a550
Marcelo Junior <designtechti.dev@protonmail.com>
2025-04-17 19:47:18 -0300

    :sparkles: Initial Commit :sparkles:

---
+++ README.md
@@ -0,0 +1,66 @@
+# Professional Portfolio
+
+![preview](github/desktop.png)
+![preview mobile](github/mobile.png)
+
+## 🚀 About this Project
+
+This is my fully responsive personal portfolio website, built using HTML, CSS, and JavaScript.
+
+## ⚡ Technologies Envolved
+
+![HTML](https://img.shields.io/badge/-HTML-121011?style=for-the-badge&logo=html5)&nbsp;
+![CSS](https://img.shields.io/badge/-CSS-121011?style=for-the-badge&logo=CSS3&logoColor=1572B6)&nbsp;
+![JavaScript](https://img.shields.io/badge/-JavaScript-121011?style=for-the-badge&logo=javascript)&nbsp;
+
+## 🪄Setup
+
+![How To Setup](github/setup.png)
+
+## 🤝 Contributing
+
+Contributions are always welcome! Here's how you can contribute:
+
+1. Fork the project
+2. Create a Branch for your Feature (`git checkout -b feature/AmazingFeature`)
+3. Add your changes (`git add .`)
+4. Commit your changes (`git commit -m 'Add some AmazingFeature'`)
+5. Push to the Branch (`git push origin feature/AmazingFeature`)
+6. Open a Pull Request
+
+## 📄 License
+
+This project is licensed under the [MIT License](./LICENSE). See the LICENSE file for more details.
+
+## 👨‍💻 Author
+
+<table width="100%">
+
+<tr>
+
+<td align="center">
+
+<a href="https://github.com/designtechti490">
+
+<img src="https://github.com/designtechti490.png" width="100px;" alt=""/>
+
+<br />
+
+<sub>
+
+<b>Marcelo Junior</b>
+          <i>Front End Developer</i>
+
+</sub>
+
+</a>
+
+</td>
+
+</tr>
+
+</table>
+
+---
+
+<p align="center"> Developed with 💜</p>
---
+++ data/user.js
@@ -0,0 +1,153 @@
+const INFO = {
+  main: {
+    title: "Marcelo Junior | Personal Portfolio",
+    name: "Marcelo Junior",
+    email: "designtechti.dev@proton.me",
+    logo: "../logo.png",
+  },
+
+  socials: {
+    twitter: "https://twitter.com/designtechtidev",
+    github: "https://github.com/designtechti490",
+    linkedin: "https://linkedin.com/designtechtidev",
+    instagram: "https://instagram.com/designtechti.dev",
+    stackoverflow: "https://stackoverflow.com/",
+    facebook: "https://facebook.com/",
+  },
+
+  homepage: {
+    title: "Front-end web and mobile app developer, and amateur astronaut.",
+    description:
+      "I am a frontend developer with expertise in React.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+  },
+
+  about: {
+    title: "I’m Marcelo Junior I live in Brazil, where I design the future.",
+    description:
+      "I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+  },
+
+  resume: {
+    education: [
+      {
+        institution: "University of California, Los Angeles",
+        degree: "PHD in Computer Science",
+        from: "2011",
+        to: "2015",
+      },
+      {
+        institution: "University of California, Los Angeles",
+        degree: "PHD in Computer Science",
+        from: "2011",
+        to: "2015",
+      },
+    ],
+
+    experience: [
+      {
+        role: "Lead Software Engineer",
+        company: "Google",
+        companylogo:
+          "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        from: "2015",
+        to: "Present",
+      },
+      {
+        role: "Senior Software Engineer",
+        company: "Facebook",
+        companylogo: "https://www.facebook.com/images/fb_icon_325x325.png",
+        from: "2011",
+        to: "2015",
+      },
+      {
+        role: "Senior Software Engineer",
+        company: "Google",
+        companylogo:
+          "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        from: "2011",
+        to: "2015",
+      },
+    ],
+
+    skills: {
+      hardSkills: [
+        "JavaScript",
+        "React",
+        "React Native",
+        "Node.js",
+        "MongoDB",
+        "Angular",
+        "Meteor.js",
+        "Laravel",
+        "PHP",
+        "C#",
+        "ASP.NET",
+        "ASP.NET Core",
+        "ASP.NET MVC",
+        "ASP",
+      ],
+      softSkills: [
+        "Problem Solving",
+        "Communication",
+        "Teamwork",
+        "Leadership",
+        "Creativity",
+      ],
+    },
+  },
+  articles: {
+    title:
+      "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+    description:
+      "Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+  },
+
+  projects: [
+    {
+      title: "Project 1",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+
+    {
+      title: "Project 2",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+
+    {
+      title: "Project 3",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+
+    {
+      title: "Project 4",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+
+    {
+      title: "Project 5",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+  ],
+};
+
+export default INFO;
