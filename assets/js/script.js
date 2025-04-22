2729f1d6d26e1d016337346ce7572e1758aa3998
Marcelo Junior <designtechti.dev@protonmail.com>
2025-04-21 21:21:39 -0300

    :rocket: Feat: User Data into Index :rocket: Adds functions to dynamically consume user data such as personal data, professional experience and education.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
-'use strict';
+import INFO from "../../data/user.js";
 
-
+("use strict");
 
 // element toggle function
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
+const elementToggleFunc = function (elem) {
+	elem.classList.toggle("active");
+};
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
-
-
+sidebarBtn.addEventListener("click", function () {
+	elementToggleFunc(sidebar);
+});
 
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
@@ -29,31 +29,29 @@ const modalText = document.querySelector("[data-modal-text]");
 
 // modal toggle function
 const testimonialsModalFunc = function () {
-  modalContainer.classList.toggle("active");
-  overlay.classList.toggle("active");
-}
+	modalContainer.classList.toggle("active");
+	overlay.classList.toggle("active");
+};
 
 // add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
+	testimonialsItem[i].addEventListener("click", function () {
+		modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+		modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+		modalTitle.innerHTML = this.querySelector(
+			"[data-testimonials-title]"
+		).innerHTML;
+		modalText.innerHTML = this.querySelector(
+			"[data-testimonials-text]"
+		).innerHTML;
 
-  testimonialsItem[i].addEventListener("click", function () {
-
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-
-    testimonialsModalFunc();
-
-  });
-
+		testimonialsModalFunc();
+	});
 }
 
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
-
-
 
 // custom select variables
 const select = document.querySelector("[data-select]");
@@ -61,59 +59,49 @@ const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+select.addEventListener("click", function () {
+	elementToggleFunc(this);
+});
 
 // add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
-
-  });
+	selectItems[i].addEventListener("click", function () {
+		let selectedValue = this.innerText.toLowerCase();
+		selectValue.innerText = this.innerText;
+		elementToggleFunc(select);
+		filterFunc(selectedValue);
+	});
 }
 
 // filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
-
-  for (let i = 0; i < filterItems.length; i++) {
-
-    if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
-      filterItems[i].classList.add("active");
-    } else {
-      filterItems[i].classList.remove("active");
-    }
-
-  }
-
-}
+	for (let i = 0; i < filterItems.length; i++) {
+		if (selectedValue === "all") {
+			filterItems[i].classList.add("active");
+		} else if (selectedValue === filterItems[i].dataset.category) {
+			filterItems[i].classList.add("active");
+		} else {
+			filterItems[i].classList.remove("active");
+		}
+	}
+};
 
 // add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
+	filterBtn[i].addEventListener("click", function () {
+		let selectedValue = this.innerText.toLowerCase();
+		selectValue.innerText = this.innerText;
+		filterFunc(selectedValue);
 
-  filterBtn[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    filterFunc(selectedValue);
-
-    lastClickedBtn.classList.remove("active");
-    this.classList.add("active");
-    lastClickedBtn = this;
-
-  });
-
+		lastClickedBtn.classList.remove("active");
+		this.classList.add("active");
+		lastClickedBtn = this;
+	});
 }
-
-
 
 // contact form variables
 const form = document.querySelector("[data-form]");
@@ -122,19 +110,15 @@ const formBtn = document.querySelector("[data-form-btn]");
 
 // add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", function () {
-
-    // check form validation
-    if (form.checkValidity()) {
-      formBtn.removeAttribute("disabled");
-    } else {
-      formBtn.setAttribute("disabled", "");
-    }
-
-  });
+	formInputs[i].addEventListener("input", function () {
+		// check form validation
+		if (form.checkValidity()) {
+			formBtn.removeAttribute("disabled");
+		} else {
+			formBtn.setAttribute("disabled", "");
+		}
+	});
 }
-
-
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
@@ -142,18 +126,139 @@ const pages = document.querySelectorAll("[data-page]");
 
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
-    }
-
-  });
+	navigationLinks[i].addEventListener("click", function () {
+		for (let i = 0; i < pages.length; i++) {
+			if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+				pages[i].classList.add("active");
+				navigationLinks[i].classList.add("active");
+				window.scrollTo(0, 0);
+			} else {
+				pages[i].classList.remove("active");
+				navigationLinks[i].classList.remove("active");
+			}
+		}
+	});
 }
+
+// Função para popular os dados do usuário
+function populateUserData() {
+	// Dados principais
+	document.title = INFO.main.title;
+	document.querySelector(".info-content .name").textContent = INFO.main.name;
+	document.querySelector('.contact-link[href^="mailto"]').textContent =
+		INFO.main.email;
+	document.querySelector('.contact-link[href^="https://wa.me/"]').textContent =
+		INFO.main.phone;
+	document.querySelector(".contact-item .location").textContent =
+		INFO.main.location;
+
+	// Links sociais
+	const socialLinks = document.querySelectorAll(".social-link");
+	socialLinks.forEach((link) => {
+		const icon = link.querySelector("ion-icon").name;
+		switch (icon) {
+			case "logo-github":
+				link.href = INFO.socials.github;
+				break;
+			case "logo-linkedin":
+				link.href = INFO.socials.linkedin;
+				break;
+			case "logo-twitter":
+				link.href = INFO.socials.twitter;
+				break;
+			case "logo-instagram":
+				link.href = INFO.socials.instagram;
+				break;
+		}
+	});
+
+	// Sobre
+	const aboutText = document.querySelector(".about-text");
+	aboutText.innerHTML = `
+    <p>${INFO.about.description}</p>
+  `;
+
+	/* // Experiência
+  const timelineList = document.querySelector(".timeline-list");
+  INFO.resume.experience.forEach((exp) => {
+    const li = document.createElement("li");
+    li.className = "timeline-item";
+    li.innerHTML = `
+      <h4 class="h4 timeline-item-title">${exp.role} at ${exp.company}</h4>
+      <span>${exp.from} — ${exp.to}</span>
+    `;
+    timelineList.appendChild(li);
+  }); */
+
+	/*   // Habilidades
+  const skillsList = document.querySelector(".skills-list");
+  INFO.resume.skills.hardSkills.forEach((skill) => {
+    const li = document.createElement("li");
+    li.className = "skills-item";
+    li.innerHTML = `
+      <div class="title-wrapper">
+        <h5 class="h5">${skill}</h5>
+        <data value="80">80%</data>
+      </div>
+      <div class="skill-progress-bg">
+        <div class="skill-progress-fill" style="width: 80%;"></div>
+      </div>
+    `;
+    skillsList.appendChild(li);
+  }); */
+}
+
+// Função para popular os dados de educação
+function populateEducation() {
+	const educationList = document.querySelector(".timeline-list");
+	// Limpa a lista atual
+	educationList.innerHTML = "";
+
+	// Adiciona cada item de educação do INFO.resume.education
+	INFO.resume.education.forEach((edu) => {
+		const li = document.createElement("li");
+		li.className = "timeline-item";
+
+		li.innerHTML = `
+      <h4 class="h4 timeline-item-title">${edu.institution}</h4>
+      <span>${edu.from} — ${edu.to}</span>
+      <p class="timeline-text">${edu.degree}</p>
+    `;
+
+		educationList.appendChild(li);
+	});
+}
+
+// Função para popular os dados de experiência
+function populateExperience() {
+	// Seleciona a lista de experiências
+	const experienceList = document.querySelector(
+		".timeline:nth-of-type(2) .timeline-list"
+	);
+
+	// Limpa o conteúdo atual
+	experienceList.innerHTML = "";
+
+	// Para cada experiência no objeto INFO
+	INFO.resume.experience.forEach((exp) => {
+		const li = document.createElement("li");
+		li.className = "timeline-item";
+
+		li.innerHTML = `
+      <h4 class="h4 timeline-item-title">${exp.company}</h4>
+      <span>${exp.from} — ${exp.to}</span>
+      <p class="timeline-text">
+        ${exp.role}
+      </p>
+    `;
+
+		experienceList.appendChild(li);
+	});
+}
+
+// Chame a função quando o DOM estiver carregado
+document.addEventListener("DOMContentLoaded", () => {
+	populateUserData();
+	populateEducation();
+	populateExperience();
+});
--- data/user.js
+++ data/user.js
@@ -1,153 +1,167 @@
 const INFO = {
-  main: {
-    title: "Marcelo Junior | Personal Portfolio",
-    name: "Marcelo Junior",
-    email: "designtechti.dev@proton.me",
-    logo: "../logo.png",
-  },
+	main: {
+		title: "Marcelo Junior | Personal Portfolio",
+		name: "Marcelo Junior",
+		email: "designtechti.dev@proton.me",
+		logo: "../logo.png",
+		location: "Porto Alegre, RS, Brazil",
+		phone: "+55 48 99209.2737",
+	},
 
-  socials: {
-    twitter: "https://twitter.com/designtechtidev",
-    github: "https://github.com/designtechti490",
-    linkedin: "https://linkedin.com/designtechtidev",
-    instagram: "https://instagram.com/designtechti.dev",
-    stackoverflow: "https://stackoverflow.com/",
-    facebook: "https://facebook.com/",
-  },
+	socials: {
+		twitter: "https://twitter.com/designtechtidev",
+		github: "https://github.com/designtechti490",
+		linkedin: "https://linkedin.com/designtechtidev",
+		instagram: "https://instagram.com/designtechti.dev",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
 
-  homepage: {
-    title: "Front-end web and mobile app developer, and amateur astronaut.",
-    description:
-      "I am a frontend developer with expertise in React.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
-  },
+	about: {
+		title:
+			"Front-end web and mobile app developer, and brazilian microentrepreneur.",
+		description:
+			"Hello, my name is Marcelo Junior, a freelancer working remotely since 2017 in the Porto Alegre region - RS, offering specialized services in Digital Marketing, Software Engineering and Technical Support. I have over 10 years of experience, including IT support, website and web application development, digital marketing (including Google Ads, paid traffic, SEO, and social media management) and sales.",
+	},
 
-  about: {
-    title: "I’m Marcelo Junior I live in Brazil, where I design the future.",
-    description:
-      "I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
-  },
+	resume: {
+		education: [
+			{
+				institution: "Rocketseat College of Technology, Online",
+				degree: "Bachelor of Technology - BTech, Software Engineering",
+				from: "Feb 2025",
+				to: "Present",
+			},
+			{
+				institution: "SENAC RS, Porto Alegre",
+				degree:
+					"Technical Course in Computer Science for the Internet, Information Technology.",
+				from: "Dec 2015",
+				to: "Sep 2017",
+			},
+			{
+				institution: "Professor Alcides Cunha State High School, Porto Alegre",
+				degree: "High School",
+				from: "Feb 2003",
+				to: "Dec 2015",
+			},
+		],
 
-  resume: {
-    education: [
-      {
-        institution: "University of California, Los Angeles",
-        degree: "PHD in Computer Science",
-        from: "2011",
-        to: "2015",
-      },
-      {
-        institution: "University of California, Los Angeles",
-        degree: "PHD in Computer Science",
-        from: "2011",
-        to: "2015",
-      },
-    ],
+		experience: [
+			{
+				role: "Software Engineer Freelancer (Self Employed)",
+				company: "Design Tech TI - Hybrid (worldwide)",
+				companylogo: "../assets/images/logo.png",
+				from: "Sep 2017",
+				to: "Present",
+			},
+			{
+				role: "IT Infraestructure Analyst",
+				company: "St. Jude Thaddeus Educational Institute - Porto Alegre",
+				companylogo: "../assets/images/clients/sao-judas-tadeu.jpeg",
+				from: "Feb 2021",
+				to: "Aug 2021",
+			},
+			{
+				role: "IT Support Specialist",
+				company: "Municipal Department of Education - Porto Alegre",
+				companylogo: "../assets/images/clients/smed.jpeg",
+				from: "Aug 2016",
+				to: "Sep 2017",
+			},
+		],
 
-    experience: [
-      {
-        role: "Lead Software Engineer",
-        company: "Google",
-        companylogo:
-          "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-        from: "2015",
-        to: "Present",
-      },
-      {
-        role: "Senior Software Engineer",
-        company: "Facebook",
-        companylogo: "https://www.facebook.com/images/fb_icon_325x325.png",
-        from: "2011",
-        to: "2015",
-      },
-      {
-        role: "Senior Software Engineer",
-        company: "Google",
-        companylogo:
-          "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-        from: "2011",
-        to: "2015",
-      },
-    ],
+		skills: {
+			hardSkills: [
+				"JavaScript",
+				"React",
+				"React Native",
+				"Node.js",
+				"MongoDB",
+				"C#",
+				"ASP.NET",
+				"ASP.NET Core",
+				"ASP.NET MVC",
+				"ASP",
+			],
+			softSkills: [
+				"Problem Solving",
+				"Communication",
+				"Teamwork",
+				"Leadership",
+				"Creativity",
+			],
+		},
+	},
 
-    skills: {
-      hardSkills: [
-        "JavaScript",
-        "React",
-        "React Native",
-        "Node.js",
-        "MongoDB",
-        "Angular",
-        "Meteor.js",
-        "Laravel",
-        "PHP",
-        "C#",
-        "ASP.NET",
-        "ASP.NET Core",
-        "ASP.NET MVC",
-        "ASP",
-      ],
-      softSkills: [
-        "Problem Solving",
-        "Communication",
-        "Teamwork",
-        "Leadership",
-        "Creativity",
-      ],
-    },
-  },
-  articles: {
-    title:
-      "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
-    description:
-      "Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-  },
+	testimonials: [
+		{
+			name: "John Doe",
+			designation: "CEO, Tech Company",
+			message:
+				"Marcelo is a talented developer who consistently delivers high-quality work. His attention to detail and problem-solving skills are impressive.",
+		},
+		{
+			name: "Jane Smith",
+			designation: "CTO, Startup Inc.",
+			message:
+				"Working with Marcelo has been a game-changer for our project. His expertise in web development has helped us achieve our goals faster than we expected.",
+		},
+	],
 
-  projects: [
-    {
-      title: "Project 1",
-      description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-      linkText: "View Project",
-      link: "https://github.com",
-    },
+	articles: {
+		title:
+			"I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
 
-    {
-      title: "Project 2",
-      description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
-      linkText: "View Project",
-      link: "https://github.com",
-    },
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
 
-    {
-      title: "Project 3",
-      description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
-      linkText: "View Project",
-      link: "https://github.com",
-    },
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
 
-    {
-      title: "Project 4",
-      description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-      linkText: "View Project",
-      link: "https://github.com",
-    },
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
 
-    {
-      title: "Project 5",
-      description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-      linkText: "View Project",
-      link: "https://github.com",
-    },
-  ],
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
 };
 
 export default INFO;
